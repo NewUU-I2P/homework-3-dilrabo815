e3956d301c2def5feeae7dcbf17f872f52326ad0
dilrabo815 <147363494+dilrabo815@users.noreply.github.com>
2023-11-14 18:20:39 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,16 +1,16 @@
 void problemSolution1(float consumed_water) {
     float cost = 13;
-    if (consumed_water >= 30) {
-        cost = cost + 30 * 0.4;
-        if (consumed_water >= 50) {
-            cost = cost + 20 * 0.12;
-            if (consumed_water >= 60) {
-                cost = cost + 10 * 1.4;
+    if (consumed_water <=30) {
+        cost = cost + (consumed_water) * 0.4;
+        if ((consumed_water >30)&& (consumed_water<=50)) {
+            cost = cost + (consumed_water-30) * 0.12;
+            if ((consumed_water >50)&&(consumed_water<= 60)) {
+                cost = cost + (consumed_water-50) * 1.4;
                 if (consumed_water>60) {
-                    cost = (consumed_water-60) * 1.5;
+                    cost = cost+(consumed_water-60) * 1.5;
                 }
             }
         }
     }
-    
+
 }
